aeb7a98da90d85d405918c35cc6ee28e3f2c6a8d
Bimal Singh <bimalsingh983@gmail.com>
2022-12-29 20:27:33 +0000

    Updates

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 def index(request):
     return render(
@@ -8,3 +10,7 @@ def index(request):
             "title": "Django example",
         },
     )
+
+
+def new(request):
+    return JsonResponse({"just": True})
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -20,6 +20,7 @@ from hello_world.core import views as core_views
 
 urlpatterns = [
     path("", core_views.index),
+    path("new/", core_views.new),
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
